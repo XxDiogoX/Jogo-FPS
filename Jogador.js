4ee05e038fc8ce576e44cbcecf2be45c29eef829
XxDiogoX <diogorodriguesk02@gmail.com>
2022-05-24 16:18:50 -0300

    Declarada a classe Jogador, que herda da classe Personagem

---
+++ Jogador.js
@@ -0,0 +1,9 @@
+import { Personagem } from "./Personagem.js";
+
+export class Jogador extends Personagem{
+    constructor(nome,vida,armaEquipada,granada){
+        super(nome,vida,armaEquipada,granada);
+
+    }
+
+}
